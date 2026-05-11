3c19f02f39d4dc8f2b76796ffaeec7f74a6b9d6b
Shreyas Agarwal <shreyasagarwal29@gmail.com>
2026-05-11 12:05:46 +0530

    speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col bg-red-500">
         <style>{`body { background-color: #000 !important; color: #fff !important; }`}</style>
         <Analytics />
+        <SpeedInsights />
         <Header />
         <main className="flex-1">{children}</main>
         <FooterRenderer />
